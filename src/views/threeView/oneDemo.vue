28c8a9b298fca442254fd9d762abec508bbff891
时不我待 <562985990@qq.com>
2022-08-29 17:20:59 +0800

    加入3d的效果

---
+++ src/views/threeView/oneDemo.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <div>
+
+    <div id="container"></div>
+
+  </div>
+
+</template>
+
+<script lang="ts" setup>
+
+import * as Three from "three";
+
+
+
+</script>
+
+<style scoped>
+
+#container {
+  height: 400px;
+
+}
+
+</style>
